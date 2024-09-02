30bd306610cc03c292c0f5d892a0a656d4920a81
Pouyan <ahmadpour.pouyan@gmail.com>
2024-09-03 00:12:01 +0200

    feat: add ui shared library

---
+++ ui-shared/src/lib/ui-shared.spec.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import UiShared from './ui-shared';
+
+describe('UiShared', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<UiShared />);
+    expect(baseElement).toBeTruthy();
+  });
+});
